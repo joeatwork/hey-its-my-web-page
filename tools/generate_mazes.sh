9a83c5cfbe2cc29bf9f7fe066dd63e6283708c02
Joe <joerbowers@gmail.com>
2019-08-21 20:22:55 -0700

    fix commit

--- tools/generate_mazes.sh
+++ tools/generate_mazes.sh
@@ -7,8 +7,8 @@
 #  400 pixels across
 #  
 
-# (20 40 80 160   320)
-# (4   8 16  32    64)
+# (10 20 40 80 160 320)
+# (1   2  4  8  16  32)
 # (64 32 16   8     4)
 # (240)
 # (48)
@@ -17,25 +17,24 @@
 
 # We also need a "half-step" between 4 and 5 : (  )
 
-for preseed in 123 345 131 222 555; do
+for seed in 122 233 344 455; do
+mseed=$seed
 for i in 1 2 3 4 5 6; do
-    seed="${preseed}${i}"
 
     width=$(echo "5*(2^$i)" | bc)
-    height=$(echo "2^($i)" | bc)
+    height=$(echo "2^($i - 1)" | bc)
     pathwidth=$(echo "256 / (2^$i)" | bc)
 
     $MAZES_DIRECTORY/grid --width=$width --height=$height | \
-       $MAZES_DIRECTORY/maze --seed=$seed | \
+       $MAZES_DIRECTORY/maze --seed=$mseed | \
        $MAZES_DIRECTORY/color --seed=$seed | \
-       $MAZES_DIRECTORY/png --path-width=$pathwidth > /tmp/mazes/${i}_${width}x${height}_${seed}.png
+       $MAZES_DIRECTORY/png --path-width=$pathwidth > /tmp/mazes/${i}_${width}x${height}_${mseed}.png
 done
 
-
-$MAZES_DIRECTORY/grid --width=240 --height=48 | \
-       $MAZES_DIRECTORY/maze --seed=$seed | \
+$MAZES_DIRECTORY/grid --width=240 --height=24 | \
+       $MAZES_DIRECTORY/maze --seed=$mseed | \
        $MAZES_DIRECTORY/color --seed=$seed | \
-       $MAZES_DIRECTORY/png --path-width=5 > /tmp/mazes/5.5_240x48_${seed}.png
+       $MAZES_DIRECTORY/png --path-width=5 > /tmp/mazes/5.5_240x24_${mseed}.png
 done
 
 optipng /tmp/mazes/*.png
